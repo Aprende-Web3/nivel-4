0744e644d70fcc106f9cf34d22ac79b553b8aeb0
Alex <tye.group.business@gmail.com>
2022-10-16 17:41:32 +0200

    update

--- meta-transactions/contracts/MetaTokenSender.sol
+++ meta-transactions/contracts/MetaTokenSender.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+//Primera Version
+/*pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
@@ -30,5 +30,52 @@ contract TokenSender {
 
     function getHash(address sender, uint amount, address recipient, address tokenContract) public pure returns (bytes32) {
         return keccak256(abi.encodePacked(sender, amount, recipient, tokenContract));
+    }
+}
+*/
+
+//Segunda version
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+contract RandomToken is ERC20 {
+    constructor() ERC20("", "") {}
+
+    function freeMint(uint amount) public {
+        _mint(msg.sender, amount);
+    }
+}
+
+contract TokenSender {
+
+    using ECDSA for bytes32;
+
+    // Nuevo mapping
+    mapping(bytes32 => bool) executed;
+
+    // Agregue el parámetro nonce aquí
+    function transfer(address sender, uint amount, address recipient, address tokenContract, uint nonce, bytes memory signature) public {
+        // Pasar por delante el nonce
+        bytes32 messageHash = getHash(sender, amount, recipient, tokenContract, nonce);
+        bytes32 signedMessageHash = messageHash.toEthSignedMessageHash();
+
+       // Requerir que esta firma aún no haya sido ejecutada
+        require(!executed[signedMessageHash], "Already executed!");
+
+        address signer = signedMessageHash.recover(signature);
+
+        require(signer == sender, "Signature does not come from sender");
+// Marcar esta firma como ejecutada ahora
+        executed[signedMessageHash] = true;
+        bool sent = ERC20(tokenContract).transferFrom(sender, recipient, amount);
+        require(sent, "Transfer failed");
+    }
+
+ // Agregue el parámetro nonce aquí
+    function getHash(address sender, uint amount, address recipient, address tokenContract, uint nonce) public pure returns (bytes32) {
+        return keccak256(abi.encodePacked(sender, amount, recipient, tokenContract, nonce));
     }
 }
--- meta-transactions/test/metatxn-test.js
+++ meta-transactions/test/metatxn-test.js
@@ -1,4 +1,6 @@
-const { expect } = require("chai");
+
+//Version 1
+/*const { expect } = require("chai");
 const { BigNumber } = require("ethers");
 const { arrayify, parseEther } = require("ethers/lib/utils");
 const { ethers } = require("hardhat");
@@ -77,5 +79,190 @@ describe("MetaTokenTransfer", function () {
 
     expect(userBalance.lt(tenThousandTokensWithDecimals)).to.be.true;
     expect(recipientBalance.gt(BigNumber.from(0))).to.be.true;
+  });
+});
+*/
+
+
+//Version 2
+const { expect } = require("chai");
+const { BigNumber } = require("ethers");
+const { arrayify, parseEther } = require("ethers/lib/utils");
+const { ethers } = require("hardhat");
+
+describe("MetaTokenTransfer", function () {
+  it("Should let user transfer tokens through a relayer with different nonces", async function () {
+  // Implementar los contratos
+    const RandomTokenFactory = await ethers.getContractFactory("RandomToken");
+    const randomTokenContract = await RandomTokenFactory.deploy();
+    await randomTokenContract.deployed();
+
+    const MetaTokenSenderFactory = await ethers.getContractFactory(
+      "TokenSender"
+    );
+    const tokenSenderContract = await MetaTokenSenderFactory.deploy();
+    await tokenSenderContract.deployed();
+
+    // Obtenga tres direcciones, trate una como la dirección del usuario
+     // uno como la dirección del repetidor y otro como la dirección del destinatario
+    const [_, userAddress, relayerAddress, recipientAddress] =
+      await ethers.getSigners();
+
+   // Mint 10,000 tokens a la dirección del usuario (para prueba)
+    const tenThousandTokensWithDecimals = parseEther("10000");
+    const userTokenContractInstance = randomTokenContract.connect(userAddress);
+    const mintTxn = await userTokenContractInstance.freeMint(
+      tenThousandTokensWithDecimals
+    );
+    await mintTxn.wait();
+
+   // Hacer que el usuario infinito apruebe el contrato del remitente del token para transferir 'RandomToken'
+    const approveTxn = await userTokenContractInstance.approve(
+      tokenSenderContract.address,
+      BigNumber.from(
+     // Este es el valor máximo de uint256 (2^256 - 1) en hexadecimal
+        "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+      )
+    );
+    await approveTxn.wait();
+
+// Haga que el usuario firme el mensaje para transferir 10 tokens al destinatario
+    let nonce = 1;
+
+    const transferAmountOfTokens = parseEther("10");
+    const messageHash = await tokenSenderContract.getHash(
+      userAddress.address,
+      transferAmountOfTokens,
+      recipientAddress.address,
+      randomTokenContract.address,
+      nonce
+    );
+    const signature = await userAddress.signMessage(arrayify(messageHash));
+
+  // Hacer que el repetidor ejecute la transacción en nombre del usuario
+    const relayerSenderContractInstance =
+      tokenSenderContract.connect(relayerAddress);
+    const metaTxn = await relayerSenderContractInstance.transfer(
+      userAddress.address,
+      transferAmountOfTokens,
+      recipientAddress.address,
+      randomTokenContract.address,
+      nonce,
+      signature
+    );
+    await metaTxn.wait();
+
+  // Verifique que el saldo del usuario haya disminuido y el destinatario haya recibido 10 tokens
+    let userBalance = await randomTokenContract.balanceOf(userAddress.address);
+    let recipientBalance = await randomTokenContract.balanceOf(
+      recipientAddress.address
+    );
+
+    expect(userBalance.eq(parseEther("9990"))).to.be.true;
+    expect(recipientBalance.eq(parseEther("10"))).to.be.true;
+// Incrementar el nonce
+    nonce++;
+
+   // Haga que el usuario firme un segundo mensaje, con un nonce diferente, para transferir 10 tokens más
+    const messageHash2 = await tokenSenderContract.getHash(
+      userAddress.address,
+      transferAmountOfTokens,
+      recipientAddress.address,
+      randomTokenContract.address,
+      nonce
+    );
+    const signature2 = await userAddress.signMessage(arrayify(messageHash2));
+  // Hacer que el repetidor ejecute la transacción en nombre del usuario
+    const metaTxn2 = await relayerSenderContractInstance.transfer(
+      userAddress.address,
+      transferAmountOfTokens,
+      recipientAddress.address,
+      randomTokenContract.address,
+      nonce,
+      signature2
+    );
+    await metaTxn2.wait();
+
+   // Verifique que el saldo del usuario haya disminuido y el destinatario haya recibido 10 tokens
+    userBalance = await randomTokenContract.balanceOf(userAddress.address);
+    recipientBalance = await randomTokenContract.balanceOf(
+      recipientAddress.address
+    );
+
+    expect(userBalance.eq(parseEther("9980"))).to.be.true;
+    expect(recipientBalance.eq(parseEther("20"))).to.be.true;
+  });
+
+  it("Should not let signature replay happen", async function () {
+  // Implementar los contratos
+    const RandomTokenFactory = await ethers.getContractFactory("RandomToken");
+    const randomTokenContract = await RandomTokenFactory.deploy();
+    await randomTokenContract.deployed();
+
+    const MetaTokenSenderFactory = await ethers.getContractFactory(
+      "TokenSender"
+    );
+    const tokenSenderContract = await MetaTokenSenderFactory.deploy();
+    await tokenSenderContract.deployed();
+// Obtenga tres direcciones, trate una como la dirección del usuario
+     // uno como la dirección del repetidor y otro como la dirección del destinatario
+ 
+    const [_, userAddress, relayerAddress, recipientAddress] =
+      await ethers.getSigners();
+
+  // Mint 10,000 tokens a la dirección del usuario (para prueba)
+    const tenThousandTokensWithDecimals = parseEther("10000");
+    const userTokenContractInstance = randomTokenContract.connect(userAddress);
+    const mintTxn = await userTokenContractInstance.freeMint(
+      tenThousandTokensWithDecimals
+    );
+    await mintTxn.wait();
+
+  // Hacer que el usuario infinito apruebe el contrato del remitente del token para transferir 'RandomToken'
+    const approveTxn = await userTokenContractInstance.approve(
+      tokenSenderContract.address,
+      BigNumber.from(
+     // Este es el valor máximo de uint256 (2^256 - 1) en hexadecimal
+        "0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+      )
+    );
+    await approveTxn.wait();
+
+   // Haga que el usuario firme el mensaje para transferir 10 tokens al destinatario
+    let nonce = 1;
+
+    const transferAmountOfTokens = parseEther("10");
+    const messageHash = await tokenSenderContract.getHash(
+      userAddress.address,
+      transferAmountOfTokens,
+      recipientAddress.address,
+      randomTokenContract.address,
+      nonce
+    );
+    const signature = await userAddress.signMessage(arrayify(messageHash));
+
+// Hacer que el repetidor ejecute la transacción en nombre del usuario
+    const relayerSenderContractInstance =
+      tokenSenderContract.connect(relayerAddress);
+    const metaTxn = await relayerSenderContractInstance.transfer(
+      userAddress.address,
+      transferAmountOfTokens,
+      recipientAddress.address,
+      randomTokenContract.address,
+      nonce,
+      signature
+    );
+    await metaTxn.wait();
+
+ // Haga que el repetidor intente ejecutar la misma transacción nuevamente con la misma firma
+     // Esta vez, esperamos que la transacción se revierta porque la firma ya se usó.
+    expect(relayerSenderContractInstance.transfer(
+      userAddress.address,
+      transferAmountOfTokens,
+      recipientAddress.address,
+      randomTokenContract.address,
+      nonce,
+      signature
+    )).to.be.revertedWith('Already executed!');
   });
 });
